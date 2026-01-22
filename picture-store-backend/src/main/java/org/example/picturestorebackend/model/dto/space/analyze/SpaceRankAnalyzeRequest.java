797cbe28164f57f1e07f12545bd3ebd005d71635
zhuyuchen <1740393870@qq.com>
2026-01-23 00:25:20 +0800

    图库分析-后端

---
+++ picture-store-backend/src/main/java/org/example/picturestorebackend/model/dto/space/analyze/SpaceRankAnalyzeRequest.java
@@ -0,0 +1,18 @@
+package org.example.picturestorebackend.model.dto.space.analyze;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 空间使用排行分析请求（仅管理员)
+ */
+@Data
+public class SpaceRankAnalyzeRequest implements Serializable {
+    /**
+     * 排名前N的空间
+     */
+    private Integer topN = 10;
+
+    private static final long serialVersionUID = 1L;
+}
